0ec435c8aa792462f373243b3d78a1385a512b43
RFMr <ruslan05eeeeeee@gmail.com>
2026-03-25 11:53:58 +0500

    Updated, auto-sync

--- MyNotes/GitAutoSynchronization/Sync_Repositories.ps1
+++ MyNotes/GitAutoSynchronization/Sync_Repositories.ps1
@@ -1,6 +1,7 @@
 # Принудительная установка кодировки UTF-8 для консоли
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 [Console]::InputEncoding = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Text.Encoding]::UTF8
 # ==============================================================================
 # КОНФИГУРАЦИЯ
 # ==============================================================================
